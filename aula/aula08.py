093cd8c0b5c84ad645686e7d3efbfaaabfb39b65
Christian P Vieira <christianpeixotovieira@gmail.com>
2026-01-20 00:22:26 -0300

    Adição da aula 08

---
+++ aula/aula08.py
@@ -0,0 +1,5 @@
+import math
+#from math import sqrt, floor - para importar somente os comandos
+num = int(input('Digite um número: '))
+raiz = math.sqrt(num)
+print('A raiz de {} é igual a {}'.format(num, raiz))
