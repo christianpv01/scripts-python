fc165d558f1f825397fe74111a32b7f3446b6e73
Christian P Vieira <christianpeixotovieira@gmail.com>
2026-01-23 21:51:55 -0300

    Modificação do Desafio 18

--- desafio/Desafio 18.py
+++ desafio/Desafio 18.py
@@ -1,11 +1,11 @@
 #Faça um programa que leia um ângulo qualquer e mostre
 #na tela o valor do seno, cosseno e tangente desse ângulo.
-import math
+from math import radians, sin, cos, tan
 print('{0} Desafio 18 {0}'.format('='*10))
 print('  Calculadora de seno, cosseno e tangente')
 print('-'*20)
-angulo = int(input(' Informe o ângulo: ')) 
-seno = math.sin(angulo)
-cosseno = math.cos(angulo)
-tangente = math.tan(angulo)
-print('O ângulo de: {}º\n Seno: {}\n Cosseno: {}\n Tangente: {}'.format(angulo,seno,cosseno,tangente))
+angulo = int(input(' Informe o ângulo: '))
+seno = sin(radians(angulo))
+cosseno = cos(radians(angulo))
+tangente = tan(radians(angulo))
+print(' Seno: {:.2f}\n Cosseno: {:.2f}\n Tangente: {:.2f}'.format(seno,cosseno,tangente))
