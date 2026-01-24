4b57131f588aa064f0f48ccd216eb6fbfb783771
Christian P Vieira <christianpeixotovieira@gmail.com>
2026-01-24 12:25:55 -0300

    Atualização dos Desafios 20, 21 e modificação do nome do arquivo mp3

--- desafio/Desafio 20.py
+++ desafio/Desafio 20.py
@@ -1,13 +1,13 @@
 #O mesmo professor do desafio anterior quer sortear a ordem de apresentação
 #de trabalhos dos alunos. Faça um programa que leia o nome dos quatro alunos
 #e mostre a ordem sorteada.
-import random
+from random import shuffle
 print('{0} Desafio 20 {0}'.format('='*10))
 print('')
 nome1 = input('Qual o nome do 1º aluno? ')
 nome2 = input('Qual o nome do 2º aluno? ')
 nome3 = input('Qual o nome do 3º aluno? ')
 nome4 = input('Qual o nome do 4º aluno? ')
-nomes = [nome1,nome2,nome3,nome4]
-fila = random.shuffle(nomes)
-print(fila)
+lista = [nome1,nome2,nome3,nome4]
+fila = shuffle(lista)
+print('A ordem da apresentação dos trabalhos vai ser ',lista)
--- desafio/Desafio 21.py
+++ desafio/Desafio 21.py
@@ -1,9 +1,9 @@
 #Faça um programa em python que abra e reproduza o áudio de um arquivo MP3.
 from audioplayer import AudioPlayer
-import time
+from time import sleep
 
-arquivo_mp3 = 'desafio/technoloyia-technologia-tecnologia.mp3'
+arquivo_mp3 = 'desafio/Desafio 21.mp3'
 player = AudioPlayer(arquivo_mp3)
 player.play()
 
-time.sleep(7)
+sleep(7)
