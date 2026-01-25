74cfc279dfa30a4f14cd1e31c62281dd55540131
Christian P Vieira <christianpeixotovieira@gmail.com>
2026-01-25 00:52:18 -0300

    Adição do complemento da aula 9 e dos enunciados dos desafios 22 a 27

--- aula/aula09d.py
+++ aula/aula09d.py
@@ -7,6 +7,8 @@ frase = 'Aprendendo Python utilizando vscode'
 #split -> Divide a string em uma lista, separado pelo padrão espaço
 print(frase.split())
 frasesplit = frase.split()
-#join -> 
+print(frasesplit[1]) #Mostrando o segundo item da lista
+print(frasesplit[1][2]) #Mostrando o terceiro caractere do segundo item da lista 
+#join -> Junção
 print('-'.join(frasesplit)) #Depois do split
 print('_'.join(frase)) #Sem split
