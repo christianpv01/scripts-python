8de24565e0e6f3c8f7740332bcc487cd172e9011
Christian P Vieira <christianpeixotovieira@gmail.com>
2026-01-25 19:44:47 -0300

    Desafio 22 pronto.

---
+++ desafio/Desafio 22.py
@@ -0,0 +1,12 @@
+#Crie um programa que leia o nome completo de uma pessoa e mostre:
+#O nome com todas as letras maiúsculas
+#O nome com todas minúsculas
+#Quantas letras ao todo(sem considerar espaços)
+#Quantas letras tem o primeiro nome
+print('{0} Desafio 22 {0}'.format('='*10))
+nome = input('Diga o seu nome completo: ')
+print('Nome em maiúsculo: ',nome.upper()) #Toda string em maiúsculo
+print('Nome em minúsculo: ',nome.lower()) #Toda string em minúsculo
+print('Quantas letras possui sem espaço: ',len(''.join(nome.split()))) #Quantidade de caractere sem considerar os espaços
+nomeSplit = nome.split()
+print('Primeiro nome: {}'.format(nomeSplit[0]))
