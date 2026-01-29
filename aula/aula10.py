bdf1b06d0654bf1ef1740f1dd115387872d69418
Christian P Vieira <christianpeixotovieira@gmail.com>
2026-01-29 20:38:39 -0300

    Adição da aula 10

---
+++ aula/aula10.py
@@ -0,0 +1,13 @@
+#Condições Simples e Compostas
+
+#if   Bloco True
+#else Bloco False
+#elif 
+
+tempo = int(input('Quanto tempo tem seu carro? '))
+'''if tempo <=3:
+    print('Seu carro é novo.')
+else:
+    print('Lata velha.')'''
+print('carro novo'if tempo<=3 else'carro velho')
+print('---Fim---')
