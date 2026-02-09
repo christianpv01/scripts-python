715927e56007f28de6cf7508dd6aae134cafb863
Christian P Vieira <christianpeixotovieira@gmail.com>
2026-02-08 22:36:57 -0300

    Desafio 36 - Resolvido

---
+++ curso-em-video-prof-gustavo-guanabara/desafio/Mundo-02/desafio-36b.py
@@ -0,0 +1,13 @@
+'''Escreva um programa para aprovar o empréstimo bancário para a compra de uma casa. Pergunte o valor da casa, o salário do comprador e em quantos anos ele vai pagar. A prestação mensal não pode exceder 30% do salário ou então o empréstimo será negado.'''
+casa = float(input('Valor da casa: R$'))
+salário = float(input('Salário do comprador: R$'))
+anos = int(input('Quantos anos de financiamento? '))
+prestação = casa / (anos * 12)
+mínimo = salário * 30 / 100
+print('Para pagar uma casa de R${:.2f} em {} anos'.format(casa,anos),end='')
+print(' a prestação será de R${:.2f}'.format(prestação))
+#print('COMPARANDO tem que pagar {:.2f} e o mínimo é de {:.2f}'.format(prestação,mínimo))
+if prestação <= mínimo:
+    print('Empréstimo pode ser CONCEDIDO!')
+else:
+    print('Empréstimo NEGADO!')
