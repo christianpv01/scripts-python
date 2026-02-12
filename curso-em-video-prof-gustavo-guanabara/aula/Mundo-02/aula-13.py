2f2c3f42019d7fab4f240b92492f3454c8989182
Christian P Vieira <christianpeixotovieira@gmail.com>
2026-02-12 20:17:50 -0300

    Aula 13

--- curso-em-video-prof-gustavo-guanabara/aula/Mundo-02/aula-13.py
+++ curso-em-video-prof-gustavo-guanabara/aula/Mundo-02/aula-13.py
@@ -22,3 +22,33 @@ for c in range(0,3):
     pula
 passo
 pega'''
+
+for c in range(0, 5): #Exclui o último número
+    print(c)
+print('Fim')
+
+for c in range(1, 6): #Exclui o último número
+    print(c)
+print('Fim')
+
+for c in range(6, 0, -1): #Exclui o último número e precisa preencher o passo negativo
+    print(c)
+print('Fim')
+
+n = int(input('Digite um número: '))
+for c in range(0, n+1):
+    print(c)
+print('Fim')
+
+i = int(input('Início: '))
+f = int(input('Fim: '))
+p = int(input('Passo: '))
+for c in range(i, f+1, p):
+    print(c)
+print('Fim')
+
+s = 0
+for c in range(0, 3):
+    n = int(input('Digite um valor: '))
+    s += n
+print('O somatório de todos os valores foi {}'.format(s))
