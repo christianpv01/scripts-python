8df646e1f25addfcf01c82cd9b87f9e937980415
Christian P Vieira <christianpeixotovieira@gmail.com>
2026-02-12 20:02:03 -0300

    Aula 13

---
+++ curso-em-video-prof-gustavo-guanabara/aula/Mundo-02/aula-13.py
@@ -0,0 +1,24 @@
+#Estruturas de repetição
+'''Laço de repetição
+        Parte 1'''
+
+''' 1º
+for c in range(1,10):
+    passo
+pega'''
+
+''' 2º
+for c in range(0,3):
+    passo
+    pula
+passo
+pega'''
+
+'''3º
+for c in range(0,3):
+    if moeda:
+        pega
+    passo
+    pula
+passo
+pega'''
