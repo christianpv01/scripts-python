aa7df44205ce857b2f6f77dac807fa3e40efbad1
Christian P Vieira <christianpeixotovieira@gmail.com>
2026-05-06 22:57:35 -0300

    Pacotes dos exercícios de módulos

---
+++ curso-em-video-prof-gustavo-guanabara/desafio/Mundo-03/pacotes/desafio107/__init__.py
@@ -0,0 +1,11 @@
+def aumentar(p, n):
+    return p + ( p * ( n / 100 ))
+
+def diminuir(p, n):
+    return p - ( p * ( n / 100 ))
+
+def dobro(n):
+    return n * 2
+
+def metade(n):
+    return n / 2
